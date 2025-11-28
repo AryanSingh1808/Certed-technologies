bbcea66fa528806aa7dd631e020f64322147b54d
AryanSingh1808 <aryansingh180804@gmail.com>
2025-11-28 14:14:02 +0530

    Updated code with latest changes

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -8,7 +8,7 @@ const { sendEnrollmentEmail } = require('../utils/emailService');
 // Initialize Razorpay instance
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 // @desc    Create Razorpay order
